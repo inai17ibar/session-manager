acf0dffd297716d21a110252126ef8ab9f0b320a
Soichiro Inatani <inai17ibar@gmail.com>
2023-12-23 17:54:31 +0900

    Sessionの情報をRedisにキャッシュできるようにした。IDは自動で振られないので手動でふった

--- go-session/go.mod
+++ go-session/go.mod
@@ -5,6 +5,10 @@ go 1.21.0
 require github.com/gorilla/sessions v1.2.2
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/rs/cors v1.10.1 // indirect
 )
--- go-session/main.go
+++ go-session/main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"context"
+	"encoding/base32"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 	"github.com/rs/cors"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var (
 	// セキュリティキーは環境変数から読み込む
-	key   = []byte(os.Getenv("SESSION_KEY"))
-	store = sessions.NewCookieStore(key)
+	key         = []byte(os.Getenv("SESSION_KEY"))
+	store       = sessions.NewCookieStore(key)
+	redisClient *redis.Client
 )
 
 func secret(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "cookie-name")
+	session, err := store.Get(r, "session-name")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		log.Printf("Error retrieving session: %v", err)
 		return
 	}
 
+	// Redisからセッションデータを読み込む
+	err = loadSessionFromRedis(session)
+	if err != nil {
+		// エラー処理
+		log.Printf("Error retrieving session: %v", err)
+		return
+	}
+
 	// セッションの値を確認
+	fmt.Println("Session ID:", session.ID)
 	fmt.Println("Session Values:", session.Values)
 
 	// 認証チェック
@@ -37,17 +56,42 @@ func secret(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "cookie-name")
+	session, err := store.Get(r, "session-name")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		log.Printf("Error retrieving session: %v", err)
 		return
 	}
 
+	if session.ID == "" {
+		// Generate a random session ID key suitable for storage in the DB
+		session.ID = string(securecookie.GenerateRandomKey(32))
+		session.ID = strings.TrimRight(
+			base32.StdEncoding.EncodeToString(
+				securecookie.GenerateRandomKey(32)), "=")
+	}
+
+	fmt.Println("Session ID:", session.ID)
+
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 7, // 1週間
+		HttpOnly: true,
+		// Secure: true, // HTTPSを使用する場合に有効化
+	}
+
 	// 認証成功と見なす
 	session.Values["authenticated"] = true
+	//test
 	session.Values["foo"] = "bar"
-	session.Values[42] = 43
+
+	// Redisにセッションデータを保存
+	err = saveSessionToRedis(session)
+	if err != nil {
+		// エラー処理
+		log.Printf("Error retrieving session: %v", err)
+		return
+	}
 
 	err = session.Save(r, w)
 	if err != nil {
@@ -57,21 +101,33 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// セッションの値を確認
+	fmt.Println("Session ID:", session.ID)
 	fmt.Println("Session Values:", session.Values)
 
 	fmt.Println("Login successful")
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "cookie-name")
+	session, err := store.Get(r, "session-name")
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		log.Printf("Error retrieving session: %v", err)
 		return
 	}
 
+	// セッションオプションの設定
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 7,
+		HttpOnly: true,
+		// Secure: true,
+	}
+
 	// ログアウト
-	session.Values["authenticated"] = false
+	//session.Values["authenticated"] = false
+	// セッションデータを削除
+	delete(session.Values, "authenticated")
+
 	err = session.Save(r, w)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -81,6 +137,96 @@ func logout(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Session Values:", session.Values)
 	fmt.Println("Logout successful")
+}
+
+func CreateToken(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	return tokenString, err
+}
+
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	return token, err
+}
+
+func init() {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Redisサーバーのアドレス
+		Password: "",               // パスワード（あれば）
+		DB:       0,                // 使用するDB
+	})
+
+	//store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+	// Redisをセッションストアとして設定するロジックをここに追加
+	//このinit関数は、プログラムが開始されると自動的に実行され、Redisクライアントを初期化します。init 関数は、定義されているパッケージ内で最初に一度だけ実行されるため、特別な呼び出しコードは必要ありません。
+}
+
+func convertSessionValuesToMap(values map[interface{}]interface{}) (map[string]interface{}, error) {
+	result := make(map[string]interface{})
+	for key, value := range values {
+		strKey, ok := key.(string)
+		if !ok {
+			return nil, fmt.Errorf("key is not a string: %v", key)
+		}
+		result[strKey] = value
+	}
+	return result, nil
+}
+
+// Redisにセッションを保存する
+func saveSessionToRedis(session *sessions.Session) error {
+	// セッションのValuesを変換
+	values, err := convertSessionValuesToMap(session.Values)
+	if err != nil {
+		return err
+	}
+
+	// JSONにシリアライズ
+	data, err := json.Marshal(values)
+	if err != nil {
+		return err
+	}
+
+	// Redisに保存
+	return redisClient.Set(context.Background(), session.ID, data, time.Duration(session.Options.MaxAge)*time.Second).Err()
+}
+
+// Redisからセッションを読み込む
+func loadSessionFromRedis(session *sessions.Session) error {
+	data, err := redisClient.Get(context.Background(), session.ID).Bytes()
+	if err != nil {
+		if err == redis.Nil {
+			return nil // セッションが存在しない場合はエラーではない
+		}
+		return err
+	}
+
+	log.Printf("Session is saved to Redis: %s\n", session.ID)
+
+	// 一時的にmap[string]interface{}型を使用
+	tempValues := make(map[string]interface{})
+	err = json.Unmarshal(data, &tempValues)
+	if err != nil {
+		return err
+	}
+
+	// session.Valuesをmap[interface{}]interface{}型に変換
+	if session.Values == nil {
+		session.Values = make(map[interface{}]interface{})
+	}
+	for k, v := range tempValues {
+		session.Values[k] = v
+	}
+
+	return nil
 }
 
 func main() {
